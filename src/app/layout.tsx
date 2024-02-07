d9cdd767d0d375b2701c2b7293a6ec6c47b4d54e
Magmals <jacopo.jm26@gmail.com>
2024-02-07 19:27:00 +0100

    Added a Server Status Check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+const serverUrl = "http://localhost:8080";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -9,14 +10,30 @@ export const metadata: Metadata = {
 	description: "",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	try {
+		if (await fetch(serverUrl + "/status").then((res) => res.status == 204))
+			return (
+				<html lang="it">
+					<body className={inter.className}>{children}</body>
+				</html>
+			);
+		else return serverUnreachable();
+	} catch (err) {
+		return serverUnreachable();
+	}
+}
+
+function serverUnreachable() {
 	return (
 		<html lang="it">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<h1>No connection to server</h1>
+			</body>
 		</html>
 	);
 }
